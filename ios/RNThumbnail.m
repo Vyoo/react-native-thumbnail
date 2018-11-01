811c4d61b0ec6d5a1dd9f5105c235ae373d65e7c
olegfilimonov <themraction@gmail.com>
2018-11-01 14:41:29 +0300

    change input to be asset url

--- ios/RNThumbnail.m
+++ ios/RNThumbnail.m
@@ -18,22 +18,22 @@ RCT_EXPORT_METHOD(get:(NSString *)filepath resolve:(RCTPromiseResolveBlock)resol
     @try {
         filepath = [filepath stringByReplacingOccurrencesOfString:@"file://"
                                                   withString:@""];
-        NSURL *vidURL = [NSURL fileURLWithPath:filepath];
-        
-        AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:vidURL options:nil];
+        NSURL *vidURL = [NSURL URLWithString:filepath];
+
+        AVURLAsset *asset = [AVURLAsset URLAssetWithURL:vidURL options:nil];
         AVAssetImageGenerator *generator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
         generator.appliesPreferredTrackTransform = YES;
-        
+
         NSError *err = NULL;
         CMTime time = CMTimeMake(1, 60);
-        
+
         CGImageRef imgRef = [generator copyCGImageAtTime:time actualTime:NULL error:&err];
         UIImage *thumbnail = [UIImage imageWithCGImage:imgRef];
         // save to temp directory
         NSString* tempDirectory = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,
                                                                        NSUserDomainMask,
                                                                        YES) lastObject];
-        
+
         NSData *data = UIImageJPEGRepresentation(thumbnail, 1.0);
         NSFileManager *fileManager = [NSFileManager defaultManager];
         NSString *fullPath = [tempDirectory stringByAppendingPathComponent: [NSString stringWithFormat:@"thumb-%@.jpg", [[NSProcessInfo processInfo] globallyUniqueString]]];
@@ -48,4 +48,4 @@ RCT_EXPORT_METHOD(get:(NSString *)filepath resolve:(RCTPromiseResolveBlock)resol
 }
 
 @end
-  
+
